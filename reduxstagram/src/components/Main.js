c41ecbd692eb086da6d5e06427da01c7b70f9a37
Lautaro Mirenda <lautaro.mirenda@celerative.com>
2017-05-26 13:43:21 -0300

    React-router

--- reduxstagram/src/components/Main.js
+++ reduxstagram/src/components/Main.js
@@ -1,11 +1,14 @@
-import {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Main extends Component {
   render(){
     return(
       <div>
-        <h1>Reduxtagram</h1>
+        <Link to="/">Reduxtagram</Link>
       </div>
     );
   }
 }
+
+export default Main;
--- reduxstagram/src/index.js
+++ reduxstagram/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.styl';
+import Main from './components/Main';
 
 
 ReactDOM.render(<div>Hola!</div>, document.getElementById('root'));
