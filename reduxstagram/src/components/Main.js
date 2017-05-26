15cc184b32c0b7099604cfac4aec99fee63c1d13
Lautaro Mirenda <lautaro.mirenda@celerative.com>
2017-05-26 13:01:55 -0300

    No me acuerdo xD.

---
+++ reduxstagram/src/components/Main.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+class Main extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Reduxtagram</h1>
+      </div>
+    );
+  }
+}
--- reduxstagram/src/index.js
+++ reduxstagram/src/index.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/style.styl';
+
+
+ReactDOM.render(<div>Hola!</div>, document.getElementById('root'));
