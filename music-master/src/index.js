e33782aee7098bf7aa62b5a58deac31e5a50dfbf
LautaroMir93 <Lautaromir93@gmail.com>
2017-05-28 16:16:04 -0300

    React-router tutorial.

--- music-master/src/index.js
+++ music-master/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 
+<div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+
 ReactDOM.render(<App />, document.getElementById('root'));
